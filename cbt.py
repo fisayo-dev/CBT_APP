78105d8785f5703542f23e67875cf3f27def25bd
fisayo-dev <olufisayobadina@gmail.com>
2023-09-09 00:47:49 +0100

    Addded Changes

--- cbt.py
+++ cbt.py
@@ -1,12 +1,25 @@
 from questions import questions
 from line import generate
+from math import fabs
+
 
 questionList = questions
+
+# Intro
+generate(30)
+print(f'Welcome to the CBT Test designed by Fisayo Obadina.  This test covers HTML5, CSS3, JavaScript and Python. Answer the question below to proceed')
+generate(30)
+
 response = input('Are you ready (y/n): ')
+response2 = input('Enter your Name: ')
 
 score = 0
 index = 0
 
+# Print Test No:
+generate(20)
+print(f'You have {len(questionList)} questions to answer:')
+generate(20)
 
 def startTest(index, score):
   currentQuestion = questionList[index]
@@ -18,6 +31,7 @@ def startTest(index, score):
       correctAnswer = opt['id']
     questionOptionsList.append({'opt': opt['opt'], 'id': opt['id']})
 
+  
   # Print Question
   generate(20)
   print(f'{index+1}. {questionTitle}')
@@ -38,8 +52,26 @@ def startTest(index, score):
   if index + 1 >= len(questionList):
     generate(20)
     generate(20)
-    scorePercentage = (score/len(questionList))*100
-    print(f'You scored {scorePercentage}%')
+    scorePercentage = fabs((score/len(questionList))*100)
+
+    scoreStatus = ''
+
+    if scorePercentage >= 90:
+      scoreStatus = 'You performed excellent'
+    elif scorePercentage >= 80:
+      scoreStatus = 'You Did well.'
+    elif scorePercentage >= 65:
+      scoreStatus = 'You are above average. But don\'t relent'
+    elif scorePercentage >= 50:
+      scoreStatus = 'You Tried, you can do better'
+    elif scorePercentage >= 30:
+      scoreStatus = 'You didn\'t have a bad score, But it could be better'
+    elif scorePercentage >= 15:
+      scoreStatus = 'Are you sure u read well for the test, try redoing.'
+    elif scorePercentage >= 8 or scorePercentage == 0:
+      scoreStatus = 'I\'m sorry to bring it to you but you failed woefully.'
+
+    print(f'Congrats {response2}, you scored {scorePercentage}%. {scoreStatus}')
     generate(20)
     generate(20)
   else:
@@ -50,6 +82,6 @@ def startTest(index, score):
 def initTest(index, score):
   startTest(index, score)
 
-
 if response == 'y' or 'Y' :
-  initTest(index, score)
+  if response != '':
+    initTest(index, score)
--- questions.py
+++ questions.py
@@ -123,6 +123,481 @@ questions = [{
       'correct': False
     },
   ]
-}]
+},{
+  'question':
+  'Which keyword is used to create a function in Python',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'def',
+      'correct': True
+    },
+    {
+      'id': 'b',
+      'opt': 'function',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'int',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'a list of items in python is called ?',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'Array',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'List',
+      'correct': True
+    },
+    {
+      'id': 'c',
+      'opt': 'Dictionary',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'When selecting a class in CSS we start with what symbol?',
+  'options': [
+    {
+      'id': 'a',
+      'opt': '.',
+      'correct': True
+    },
+    {
+      'id': 'b',
+      'opt': '#',
+      'correct': False
+    },
+    {
+      'id': '@',
+      'opt': 'I don\'t know',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'Which of the following is not a programing Language',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'Rust',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'Scratch',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'Python',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Anakonda',
+      'correct': True
+    },
+  ]
+},{
+  'question': 
+  'Which syntax best explians how to select the first item in an array in javascript',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'books[1]',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'books[2]',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'books[3]',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'books[0]',
+      'correct': True
+    },
+  ]
+},{
+  'question':
+  'The len() in python gives the length of a list',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'True',
+      'correct': True
+    },
+    {
+      'id': 'b',
+      'opt': 'False',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'I don\'t know',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'which of the following keyword is not used to define a variable in javascript',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'var',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'set',
+      'correct': True
+    },
+    {
+      'id': 'c',
+      'opt': 'const',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'let',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'What\'s the differnece between const and let keyword in javascript',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'let does not allow to reassign the value while const allows',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'let and const allow to reassign the value',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'const does not allow to reassign the value while let allows',
+      'correct': True
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'Which of the following code is the correct syntax of creating an array in javascript',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'const books = ["Hang\'s man Curse","Keys to Heaven","Introduction to Python"]',
+      'correct': True
+    },
+    {
+      'id': 'b',
+      'opt': 'const books = [Hang\'s man Curse,Keys to Heaven,Introduction to Python]',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'const books = ["Hang\'s man Curse","Keys to Heaven"."Introduction to Python"]',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'const books = ["Hang\'s man Curse",Keys to Heaven,Introduction to Python"]',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'Which of the following Syntax is correct to create a function in python',
+  'options': [
+    {
+      'id': 'a',
+      'opt': ' function funcName() {}',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'def funcName():',
+      'correct': True
+    },
+    {
+      'id': 'c',
+      'opt': 'function funcName():',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'def funcName() {fdfd}',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'Is there any difference between the background and background-image property in CSS',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'Not really',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'No',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'Yes',
+      'correct': True
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me !!',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'Which of the following is correct ?',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'const is for python',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'def is for javascript',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'array is for python',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'list is for python',
+      'correct': True
+    },
+  ]
+},{
+  'question':
+  'Which of the following code syntax is correct in the Python Language',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'print("Text)',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'if else age < 4: #Run code',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'if footballers.length > 4',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'elif age[0] == False',
+      'correct': True
+    },
+  ]
+},{
+  'question':
+  'Does Python use switch statement',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'Yes',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'No',
+      'correct': True
+    },
+    {
+      'id': 'c',
+      'opt': 'I don\'t know',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'Leave me alone',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'apples = ["mango","banana","watermelon","pawpaw"]                                            print(apples[{len(apples) - 1}]) ----  What will be the output of the following code syntax',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'banana',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'watermelon',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'mango',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'pawpaw',
+      'correct': True
+    },
+  ]
+},{
+  'question':
+  'ronaldo["name"]. ronaldo is a varaible. What is it\'s datatype',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'string',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'boolean',
+      'correct': True
+    },
+    {
+      'id': 'c',
+      'opt': 'dictionary',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'object',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'The correct syntax to import a function from a file/module in python is ?',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'import function from module',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'from function import module',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'import module from function',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'from module import function',
+      'correct': True
+    },
+  ]
+},{
+  'question':
+  'A Curlybrace is indicated by a what ? ',
+  'options': [
+    {
+      'id': 'a',
+      'opt': '{}',
+      'correct': True
+    },
+    {
+      'id': 'b',
+      'opt': '[]',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': '()',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': '<>',
+      'correct': False
+    },
+  ]
+},{
+  'question':
+  'The difference between a set and a list in Python is that ?',
+  'options': [
+    {
+      'id': 'a',
+      'opt': 'Set stores data while list stores only numbers',
+      'correct': False
+    },
+    {
+      'id': 'b',
+      'opt': 'sets stores strings but list stored only integers',
+      'correct': False
+    },
+    {
+      'id': 'c',
+      'opt': 'set allows data to be repeated while a list does not',
+      'correct': False
+    },
+    {
+      'id': 'd',
+      'opt': 'set does not permit you to chnage it\'s data directly while a list allows you to change it\'s data',
+      'correct': True
+    },
+  ]
+},]
 
 
